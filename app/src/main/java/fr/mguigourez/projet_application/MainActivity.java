4cff641ffde6cc0d7ff9a834ccaf7a54bcbef48f
Maxime <maxime.guigourez@gmail.com>
2019-03-19 15:42:45 +0100

    Page d'accueil de recherche + bouton search / affichages nombre des pages

--- app/src/main/java/fr/mguigourez/projet_application/MainActivity.java
+++ app/src/main/java/fr/mguigourez/projet_application/MainActivity.java
@@ -2,12 +2,34 @@ package fr.mguigourez.projet_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText studio;
+    private EditText date;
+    private Spinner genre;
+    private SeekBar nombreP;
+    private TextView affichageP;
+    private ImageButton search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.studio = findViewById(R.id.studio);
+        this.date = findViewById(R.id.date);
+        this.genre = findViewById(R.id.genre);
+        this.nombreP = findViewById(R.id.nombre_pages);
+        this.affichageP = findViewById(R.id.affichage_pages);
+        this.search = findViewById(R.id.recherche);
+
+
+
     }
 }
