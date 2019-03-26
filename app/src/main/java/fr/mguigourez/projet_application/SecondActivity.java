c6cc682139226bfe5139faeaa109ef9896bd9979
Maxime <maxime.guigourez@gmail.com>
2019-03-26 16:21:03 +0100

    troisieme failed

--- app/src/main/java/fr/mguigourez/projet_application/SecondActivity.java
+++ app/src/main/java/fr/mguigourez/projet_application/SecondActivity.java
@@ -1,7 +1,11 @@
 package fr.mguigourez.projet_application;
 
+        import android.content.Intent;
+        import android.graphics.drawable.ColorDrawable;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
         import android.widget.EditText;
         import android.widget.ImageButton;
         import android.widget.LinearLayout;
@@ -11,24 +15,40 @@ package fr.mguigourez.projet_application;
 
 public class SecondActivity extends AppCompatActivity {
 
-    LinearLayout layout=( LinearLayout ) findViewById(R.id.linear_view_button);
-
+    private LinearLayout listeWithScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        for( int i = 0; i < 20; i++ ){
+        this.listeWithScroll = findViewById(R.id.liste);
+        Button Test_Third = new Button(this);
+        Test_Third.setText("Troisième => ");
+        this.listeWithScroll.addView(Test_Third);
 
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(0,5,0,0);
-            TextView text = new TextView (this);
-            text.setText(i);
-            text.setLayoutParams(params);
-            layout.addView(text);
+        Test_Third.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent details = new Intent(getApplicationContext(),ThirdActivity.class);
+                startActivity(details);
+            }
+        });
 
+
+
+        for(int i = 0; i<50;i++){
+
+            TextView ProgrammaticallyTextView = new TextView(this);
+            ProgrammaticallyTextView.setText(" Liste "+i);
+            ProgrammaticallyTextView.setTextSize(25);
+
+            this.listeWithScroll.addView(ProgrammaticallyTextView);
         }
+
+
+
+
 
 
     }
